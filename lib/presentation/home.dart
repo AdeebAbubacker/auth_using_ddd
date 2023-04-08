35922e70c34f1597c92e97bee77019829706a275
Adeeb Abubacker <adeebabu655@gmail.com>
2023-04-08 12:00:00 +0530

    third commit

--- lib/presentation/home.dart
+++ lib/presentation/home.dart
@@ -5,6 +5,6 @@ class Home extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(body: SafeArea(child: Column(children: [],)),);
+    return  Scaffold(body: SafeArea(child: Column(children: [Container()],)),);
   }
 }
