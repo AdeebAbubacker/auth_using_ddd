2aa2cfcea66bc66831e2b90cb076e45676254a69
Adeeb Abubacker <adeebabu655@gmail.com>
2023-11-13 00:43:01 +0530

    third commit

--- lib/presentation/login.dart
+++ lib/presentation/login.dart
@@ -29,7 +29,7 @@ class Login extends StatelessWidget {
       print(token);
       print('Login successful!');
     } else {
-      // Handle login failure
+
       // ignore: avoid_print
       print('Login failed: ${response.body}');
     }
